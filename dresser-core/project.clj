78be3de405764065a539504a12fc7633968f1fad
Frozenlock <frozenlock@gmail.com>
2023-08-03 21:07:20 -0400

    [core] Remove some test dependencies

--- dresser-core/project.clj
+++ dresser-core/project.clj
@@ -2,9 +2,7 @@
   :description "Storage abstraction layer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [missionary "b.31"]
-                 [com.hyperfiddle/electric "v2-alpha-349-ge9996713"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :test-paths ["src" "test"]
   :repl-options {:init-ns dresser.base}
   :profiles {:test {:injections [(do (require 'dresser.test)
