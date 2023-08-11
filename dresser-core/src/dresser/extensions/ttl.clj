5b7a8d4095ef3881a861d093d59d20df832e9e58
Frozenlock <frozenlock@gmail.com>
2023-08-11 11:26:16 -0400

    [core] Leverage ID field in TTL extension (100x+ speed increase)
    To further increase speed, the expiration time is encoded in the
    ttl-drawer document ID.  Searching for expired documents is thus
    searching for IDs 'smaller than'. This takes advantage of the fact
    that the ID (the primary key) is the most likely field to be
    optimized. With the Codax implementation, this optimization lowers
    the time taken to search 20k documents from ~500ms to <5ms.

--- dresser-core/src/dresser/extensions/ttl.clj
+++ dresser-core/src/dresser/extensions/ttl.clj
@@ -8,13 +8,19 @@
 
 ;; Rather than storing the expiration as a field in the target
 ;; document, we store the document reference and the expiration in a
-;; different drawer. This allows for a much easier retrieval. (Fetch
-;; on a single drawer rather than searching all documents everywhere.)
-
-;; This is still a bad design IMO, fetch/where is slow. Consider
-;; adding 'partitioned drawers' for much faster resolution.
+;; different drawer (ttl-drawer). This allows for a much easier
+;; retrieval as we fetch on a single drawer rather than searching all
+;; documents everywhere.
+;;
+;; To further increase speed, the expiration time is encoded in the
+;; ttl-drawer document ID.  Searching for expired documents is thus
+;; searching for IDs 'smaller than'. This takes advantage of the fact
+;; that the ID (the primary key) is the most likely field to be
+;; optimized. With the Codax implementation, this optimization lowers
+;; the time taken to search 20k documents from ~500ms to <5ms.
 
 (def ttl-drawer :drs_ttl)
+(def ttl-field :drs_ttl)
 
 (defn ms [inst]
   (.getTime inst))
@@ -65,17 +71,31 @@
     (or (zero? ret)
         (pos? ret))))
 
+(defn- ttl-id
+  "Generates an ID prefixed with the expiration time in ms.
+  This makes it easy to sort and leverage the fact that the ID
+  field (primary key) is the most likely to be optimized/fast."
+  [dresser inst]
+  (db/tx-let [tx dresser]
+      [id (db/gen-id! tx ttl-drawer)]
+      (str (ms inst) ":" id)))
+
 (defn upsert-expiration!
   "Sets the document expiration and returns the ref.
-  nil removes any existing expiration."
+  nil removes any existing expiration.  The expiration is the time
+  after which the document is *eventually* deleted."
   [dresser doc-ref inst]
   (assert (or (nil? inst)
               (inst? inst)))
   (db/tx-let [tx dresser]
-      [ttl-id (str "drs_ttl-" (refs/doc-id doc-ref))] ; deterministic ID from doc-ref
+      [new-ttl-id (when inst (ttl-id tx inst))
+       ?existing-ttl-id (refs/get-at tx doc-ref [ttl-field])
+       _ (db/delete! tx ttl-drawer ?existing-ttl-id)]
     (-> (if (nil? inst)
-          (db/delete! tx ttl-drawer ttl-id)
-          (db/upsert! tx ttl-drawer {:id ttl-id, :exp inst, :target doc-ref}))
+          (refs/dissoc-at! tx doc-ref [] ttl-field)
+          (-> tx
+              (db/upsert! ttl-drawer {:id new-ttl-id :target doc-ref})
+              (refs/assoc-at! doc-ref [ttl-field] new-ttl-id)))
         (db/with-result doc-ref))))
 
 ;; TODO: convert into multiple transactions.
@@ -83,7 +103,7 @@
 (defn delete-expired!
   [dresser]
   (db/tx-let [tx dresser]
-      [ttl-docs (db/fetch tx ttl-drawer {:where {:exp {db/lt (now)}}
+      [ttl-docs (db/fetch tx ttl-drawer {:where {:id {db/lt (str (ms (now)))}}
                                          :only  [:target :id]})]
     ;; This reduce is kind of a pain.
     ;; Perhaps a macro to make this easier?
@@ -93,11 +113,15 @@
             tx ttl-docs)))
 
 (defn add-with-ttl!
+  "Adds a document with a time to live and returns its ref.
+  The time to live sets the time after which the document is
+  *eventually* deleted."
   [dresser drawer data ttl-ms]
   (db/tx-let [tx dresser]
-      [d-ref (refs/add! tx drawer data)]
-    (-> (upsert-expiration! tx d-ref (now ttl-ms))
-        (db/with-result d-ref))))
+      [new-ttl-id (ttl-id tx (now ttl-ms))
+       doc-ref (refs/add! tx drawer (assoc data ttl-field new-ttl-id))
+       _ (db/upsert! tx ttl-drawer {:id new-ttl-id :target doc-ref})]
+    doc-ref))
 
 (ext/defext ttl
   [delay-between-delete-ms]
