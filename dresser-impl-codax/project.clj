c5d9a02449494ce15df74c580dffb19784e4b015
Frozenlock <frozenlock@gmail.com>
2024-02-14 19:58:13 -0500

    [codax] clean replikativ/hasch dependencies

--- dresser-impl-codax/project.clj
+++ dresser-impl-codax/project.clj
@@ -6,7 +6,18 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojars.frozenlock/dresser "0.1.0-SNAPSHOT"]
                  [codax "1.4.0"]
-                 [io.replikativ/hasch "0.3.94"]]
+
+                 ;; hashc has a messy deps tree
+                 [io.replikativ/hasch "0.3.94"
+                  :exclusions [io.replikativ/incognito
+                               org.clojure/clojurescript]]
+                 [io.replikativ/incognito "0.3.66"
+                  :exclusions [com.cognitect/transit-clj
+                               com.cognitect/transit-cljs
+                               fress
+                               org.clojure/data.fressian
+                               org.clojure/clojurescript
+                               org.clojure/tools.cli]]]
   :plugins [[lein-modules "0.3.11"]]
   :test-paths ["src" "test"]
   :repl-options {:init-ns dresser.impl.codax})
